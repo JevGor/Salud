aaaa80fb3f45fa56171e3528195935f81f712657
Akim87 <akim3496389@gmail.com>
2020-01-19 19:55:32 +0200

    text added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 /* FADED TEXT*/
-document.body.addEventListener(
-    'click',
-    (event) => {
-        if (event.target.classList.contains('read-more')) {
-            event.preventDefault;
-            console.log(event.target.previousElementSibling)
-            event.target.previousElementSibling.classList.toggle('faded-txt')
-            event.target.previousElementSibling.style.setAttribute='max-heigh = 100%';
-        }
+document.body.addEventListener("click", event => {
+  if (event.target.classList.contains("read-more")) {
+    event.preventDefault;
+    event.target.previousElementSibling.classList.toggle("faded-txt");
+  }
+});
+
+/* LANGUAGE */
+
+const select = document.querySelector(".items");
+const langTxt = document.querySelectorAll(".lang");
+
+select.addEventListener("change", event => {
+  langTxt.forEach(element => {
+    if (element.classList.contains(`${event.target.value}`)) {
+      element.classList.remove("d-none");
+    } else {
+      element.classList.add("d-none");
     }
-);
+  });
+});
