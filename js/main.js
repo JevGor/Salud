8fd095fc9bab0e4cdcd0f6dc31144b0b8a2f48f0
Akim87 <akim3496389@gmail.com>
2020-01-23 19:24:33 +0300

    popup

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 /* FADED TEXT*/
-document.body.addEventListener("click", event => {
-  if (event.target.classList.contains("read-more")) {
+const popup = document.querySelector('.popup');
+
+const initPopup = (event) => {
+	if (event.target.classList.contains('read-more')) {
     event.preventDefault;
-    event.target.previousElementSibling.classList.toggle("faded-txt");
-  }
-});
+    popup.classList.remove('d-none');
+		popup.innerHTML = event.target.previousElementSibling.innerHTML;
+
+		document.body.classList.add('popup-wrapper');
+	}
+	document.body.addEventListener('click', closePopup, { once: true });
+};
+
+const closePopup = (event) => {
+	if (event.target.closest('div') != popup) {
+		popup.classList.add('d-none');
+		document.body.classList.remove('popup-wrapper');
+	}
+};
+
+document.body.addEventListener('click', initPopup);
 
 /* LANGUAGE */
 
-const select = document.querySelector(".items");
-const langTxt = document.querySelectorAll(".lang");
+const select = document.querySelector('.items');
+const langTxt = document.querySelectorAll('.lang');
 
-select.addEventListener("change", event => {
-  langTxt.forEach(element => {
-    if (element.classList.contains(`${event.target.value}`)) {
-      element.classList.remove("d-none");
-    } else {
-      element.classList.add("d-none");
-    }
-  });
+select.addEventListener('change', (event) => {
+	langTxt.forEach((element) => {
+		if (element.classList.contains(`${event.target.value}`)) {
+			element.classList.remove('d-none');
+		} else {
+			element.classList.add('d-none');
+		}
+	});
 });
