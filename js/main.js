cd238f8893cf8870f469aa1f8f7e917b259c6e83
Akim87 <akim3496389@gmail.com>
2020-02-07 16:39:16 +0300

    mobile menu animation fix

--- js/main.js
+++ js/main.js
@@ -1,126 +1,126 @@
-'use strict';
+/* eslint-disable no-tabs */
+'use strict'
 
 class Popup {
-  constructor(element) {
-    this.elem = document.querySelector(element);
-    this.isOpen = false;
+  constructor (element) {
+    this.elem = document.querySelector(element)
+    this.isOpen = false
   }
 
-  openPopup() {
-    this.elem.classList.add('open');
-    this.isOpen = true;
+  openPopup () {
+    this.elem.classList.add('open')
+    this.isOpen = true
   }
 
-  closePopup() {
-    this.elem.classList.remove('open');
-    this.isOpen = false;
+  closePopup () {
+    this.elem.classList.remove('open')
+    this.isOpen = false
   }
 
-  togglePopup() {
-    if (this.isOpen) this.closePopup();
-    else this.openPopup();
+  togglePopup () {
+    if (this.isOpen) this.closePopup()
+    else this.openPopup()
   }
 
-  setValue(title, value) {
-    this.elem.querySelector('.title').innerHTML = title;
+  setValue (title, value) {
+    this.elem.querySelector('.title').innerHTML = title
   }
 }
 
 /* Language select */
-const langPopup = new Popup('.lang-list');
-const langTxt = document.querySelectorAll('.lang');
-const langSelectItems = document.querySelectorAll('.lang-item');
+const langPopup = new Popup('.lang-list')
+const langTxt = document.querySelectorAll('.lang')
+const langSelectItems = document.querySelectorAll('.lang-item')
 
 const setLang = () => {
   langTxt.forEach((element) => {
     if (element.classList.contains(`${event.target.dataset.value}`)) {
-      element.classList.remove('d-none');
+      element.classList.remove('d-none')
     } else {
-      element.classList.add('d-none');
+      element.classList.add('d-none')
     }
-  });
-};
+  })
+}
 
 const setMobLangBtnActive = () => {
   langSelectItems.forEach((element) => {
     if (element.dataset.value === event.target.dataset.value) {
-      element.classList.add('lang-active');
+      element.classList.add('lang-active')
     } else {
-      element.classList.remove('lang-active');
+      element.classList.remove('lang-active')
     }
-  });
-};
+  })
+}
 
 const selectLang = () => {
-  document.querySelector('.title').innerHTML = event.target.innerHTML;
-  setLang();
-};
+  document.querySelector('.title').innerHTML = event.target.innerHTML
+  setLang()
+}
 
 const onSelectLangClick = () => {
   if (event.target.className === 'title') {
-    langPopup.togglePopup();
+    langPopup.togglePopup()
   } else if (event.target.classList.contains('lang-item')) {
-    selectLang();
-    setMobLangBtnActive();
-    langPopup.closePopup();
+    selectLang()
+    setMobLangBtnActive()
+    langPopup.closePopup()
   } else if (!langPopup.elem.contains(event.target)) {
-    langPopup.closePopup();
+    langPopup.closePopup()
   }
-};
+}
 
-document.addEventListener('click', onSelectLangClick);
+document.addEventListener('click', onSelectLangClick)
 
 /* Article Popup */
-const textPopup = new Popup('.popup');
-const popupContainer = document.querySelector('.popup');
+const textPopup = new Popup('.popup')
+const popupContainer = document.querySelector('.popup')
 
 const openTextPopup = () => {
-  popupContainer.innerHTML = event.target.previousElementSibling.innerHTML;
-  textPopup.openPopup();
-  document.body.classList.add('popup-wrapper');
-};
+  popupContainer.innerHTML = event.target.previousElementSibling.innerHTML
+  textPopup.openPopup()
+  document.body.classList.add('popup-wrapper')
+}
 
 const closeTextPopup = () => {
-  textPopup.closePopup();
-  document.body.classList.remove('popup-wrapper');
-};
+  textPopup.closePopup()
+  document.body.classList.remove('popup-wrapper')
+}
 
 const onReadMoreClick = () => {
   if (event.target.classList.contains('read-more')) {
-    openTextPopup();
+    openTextPopup()
   } else if (!textPopup.elem.contains(event.target)) {
-    closeTextPopup();
+    closeTextPopup()
   }
-};
+}
 
-document.addEventListener('click', onReadMoreClick);
+document.addEventListener('click', onReadMoreClick)
 
 /* Mobile Menu */
-const mobMenuPopup = new Popup('.mob-menu');
-const mobMenuBtn = document.querySelector('.mob-menu-btn');
+const mobMenuPopup = new Popup('.mob-menu')
+const mobMenuBtn = document.querySelector('.mob-menu-btn')
 
 const toggleMobMenu = () => {
-  mobMenuBtn.classList.toggle('open');
-  mobMenuPopup.togglePopup();
-};
+  mobMenuBtn.classList.toggle('open')
+  mobMenuPopup.togglePopup()
+}
 
 const closeMobMenu = () => {
-  mobMenuBtn.classList.remove('open');
-  mobMenuPopup.closePopup();
-};
+  mobMenuBtn.classList.remove('open')
+  mobMenuPopup.closePopup()
+}
 
 const onMobMenuBtnClick = () => {
   if (event.target.closest('.mob-menu-btn')) {
-    toggleMobMenu();
+    toggleMobMenu()
   } else if (
-    !mobMenuPopup.elem.contains(event.target) ||
     event.target.classList.contains('mob-nav-link') ||
-    event.target.classList.contains('lang-item') ||
-    !!event.target.closest('.socials-link') ||
-    !!event.target.closest('.mob-menu-btn')
+		event.target.classList.contains('lang-item') ||
+		event.target.closest('.socials-link') ||
+		event.target.closest('.mob-menu-btn')
   ) {
-    closeMobMenu();
+    closeMobMenu()
   }
-};
+}
 
-document.addEventListener('click', onMobMenuBtnClick);
+document.body.addEventListener('click', onMobMenuBtnClick)
